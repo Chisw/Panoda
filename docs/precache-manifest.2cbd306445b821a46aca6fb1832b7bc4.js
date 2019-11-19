61bd40fdcd12d36d64cbff4ba6df8fa036280bca
Chisw <chisw@qq.com>
2019-11-19 08:30:56 +0800

    set github pages

---
+++ docs/precache-manifest.2cbd306445b821a46aca6fb1832b7bc4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e7798e2bf0b0b465a9420451a865e65",
+    "url": "/i/panoda/index.html"
+  },
+  {
+    "revision": "2ffb79904bd8728c4fa2",
+    "url": "/i/panoda/static/css/2.07d04ac3.chunk.css"
+  },
+  {
+    "revision": "5f8820dd47baa9569853",
+    "url": "/i/panoda/static/css/main.b6ff884e.chunk.css"
+  },
+  {
+    "revision": "2ffb79904bd8728c4fa2",
+    "url": "/i/panoda/static/js/2.bbd49a1f.chunk.js"
+  },
+  {
+    "revision": "5f8820dd47baa9569853",
+    "url": "/i/panoda/static/js/main.bb2cfc1c.chunk.js"
+  },
+  {
+    "revision": "a2bc6e6974f7225d014a",
+    "url": "/i/panoda/static/js/runtime~main.1950d58c.js"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "/i/panoda/static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "/i/panoda/static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "/i/panoda/static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "/i/panoda/static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "/i/panoda/static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "/i/panoda/static/media/icons-20.90247cfd.ttf"
+  },
+  {
+    "revision": "0e27753c8082c4b434babef16036fe91",
+    "url": "/i/panoda/static/media/mono.0e27753c.woff"
+  },
+  {
+    "revision": "391098f45ce90d8bfaa3fada0a01a140",
+    "url": "/i/panoda/static/media/pin.391098f4.svg"
+  },
+  {
+    "revision": "22c89e3f93082fcbbebe11eed7b04c31",
+    "url": "/i/panoda/static/media/rect.22c89e3f.svg"
+  }
+]);
