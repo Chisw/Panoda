23d5bbb910b3fac4822db4c9f474b9eb373b9f2d
Chisw <chisw@qq.com>
2020-05-25 09:30:43 +0800

    pure tailwind

--- src/components/panels/About.tsx
+++ src/components/panels/About.tsx
@@ -100,7 +100,7 @@ export default function About() {
           [
             { label: 'BaiduMap', link: 'http://lbsyun.baidu.com' },
             { label: 'Blueprint', link: 'https://blueprintjs.com' },
-            { label: 'TailwindCSS', link: 'https://next.tailwindcss.com' },
+            { label: 'TailwindCSS', link: 'https://tailwindcss.com/' },
             { label: 'JSZip.js', link: 'https://github.com/Stuk/jszip' },
             { label: 'FileSaver.js', link: 'https://github.com/eligrey/FileSaver.js' },
             { label: 'Piexifjs', link: 'https://github.com/hMatoba/piexifjs' },
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: false,
+  purge: [
+    './src/**/*.tsx',
+  ],
   theme: {
     extend: {},
   },
